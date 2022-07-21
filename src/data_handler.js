ffd507a8af221b4d45fd103d3df5e6d5615455cf
Owen Lee <owentheturkey@gmail.com>
2022-07-22 02:14:28 +0800

    modified:   src/data_handler.js

--- src/data_handler.js
+++ src/data_handler.js
@@ -4,21 +4,22 @@ const { MessageEmbed, EmbedBuilder } = require("discord.js");
 //Get data from data_fetcher.fetch_data
 async function create_player_embed(data){
     //Somehow + is faster for combining strings than concat so...
-
     let arwr = ("AR: " + await get_base_info(data, Option="ar_level") + " --- WR: " + await get_base_info(data, Option="world_level"));
-    //const playerEmbed = new EmbedBuilder()
     let signature = await get_base_info(data, Option="signature")
+    //In the case of no signatures
     if (signature == undefined){
         signature = "*no signature"
     }
     let nickname = await get_base_info(data, Option="nickname")
+    let spiral = await get_base_info(data, Option="abyss_floor") + "-" + await get_base_info(data, Option="abyss_chamber")
 
     const playerEmbed = new EmbedBuilder()
     .setColor('#29cf84')
     .setTitle(nickname)
     .setDescription(signature)
     .addFields([
-        { name: "Adventure Rank --- WR", value: `${arwr}` || "Bruh",}
+        { name: "Adventure Rank --- WR", value: `${arwr}` || "Error",},
+        { name: "Highest Spiral Abyss (Patch 2.8)", value: `${spiral}` || "Error" }
     ]);
     
     return playerEmbed
