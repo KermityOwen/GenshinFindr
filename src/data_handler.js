2e1f39d0beef81c968edbfa9ef42aac35b2ed326
Owen Lee <owentheturkey@gmail.com>
2022-07-23 02:23:59 +0800

    added char mapping and modified to include icon

--- src/data_handler.js
+++ src/data_handler.js
@@ -15,7 +15,7 @@ async function create_player_embed(data){
     let spiral = await get_base_info(data, option="abyss_floor") + "-" + await get_base_info(data, option="abyss_chamber")
     let achievements = await get_base_info(data, option="achievements")
 
-    let prof_pic = char_img_map[await get_base_info(data, option="prof_pic")].SideIconName
+    let prof_pic = char_img_map[await get_base_info(data, option="prof_pic")].IconName
     let prof_pic_url = "https://enka.network/ui/" + prof_pic + ".png"
 
     const playerEmbed = new EmbedBuilder()
@@ -44,5 +44,26 @@ fetch_data(700378769).then(r => {
         console.log(r2)
     })
     
+})
+
+
+const fs = require('fs');
+
+for (let i = 10000002; i<10000066; i++){
+    try{
+        char_img_map[i].IconName = char_img_map[i].SideIconName.replace('Side_','')
+    } catch (e) {
+        //console.log(i)
+    }
+}
+
+fs.writeFile("../resources/character_mapping.json", JSON.stringify(char_img_map, null, 4), 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+
+    console.log(char_img_map)
+    console.log("JSON file has been saved.");
 })
 */
